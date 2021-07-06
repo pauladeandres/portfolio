e5bef0ea98727160bdb43dcf19f967a4a168b87d
paula <p.andrescaldevilla@gmail.com>
2021-07-06 13:38:19 +0200

    hash

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
-  <Router basename="https://pauladeandres.github.io/portfolio">
+  <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
